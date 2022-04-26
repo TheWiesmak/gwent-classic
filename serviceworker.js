a412df040179723ada0f3f1a74ed469ffe6d833e
Gerard Gajda <54535486+TheWiesmak@users.noreply.github.com>
2022-04-26 12:46:56 +0200

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@ var staticCacheName = "pwa";
 self.addEventListener("install", function (e) {
 e.waitUntil(
 	caches.open(staticCacheName).then(function (cache) {
-	return cache.addAll(["/gwent-classic"]);
+	return cache.addAll(["/"]);
 	})
 );
 });
